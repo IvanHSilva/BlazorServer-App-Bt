ff105352649b598d8145d436f0f2b4c82fffddb6
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-05-16 10:23:55 -0300

    EFCore added

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,16 @@
-﻿namespace BlazingShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazingShop.Models;
 
 public class Category {
+    [Key]
+    [Required(ErrorMessage = "Informe o Id")]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o Nome")]
     public string Name { get; set; } = string.Empty;
+
+    public List<Product> Products { get; set; } = new();
 
     public Category(){}
 
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,24 @@
-﻿namespace BlazingShop.Models; 
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazingShop.Models; 
 
 public class Product {
+    [Key]
+    [Required(ErrorMessage = "Informe o Id")]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o Nome")] 
     public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Informe a Descrição")] 
     public string Description { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Informe a Imagem")] 
     public string Image { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Informe o Preço")]
     public double Price { get; set; } 
+    
     public int CategoryId { get; set; }
     public Category Category { get; set; } = new();
 
