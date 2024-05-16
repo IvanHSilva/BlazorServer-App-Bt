a68dfde32ebe177a538a0f2287ca00c59146df39
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-05-16 11:22:15 -0300

    Product Insert Page

--- Models/Product.cs
+++ Models/Product.cs
@@ -20,6 +20,7 @@ public class Product {
     public double Price { get; set; } 
     
     public int CategoryId { get; set; }
+
     public Category Category { get; set; } = new();
 
     public Product() { }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using BlazingShop.Models.Data;
+using BlazingShop.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
