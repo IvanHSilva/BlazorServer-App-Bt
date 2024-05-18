0da4b5ca2c11553d69983e030e3bdb69fe378374
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-05-18 11:18:41 -0300

    Product Form Validation

--- Models/Product.cs
+++ Models/Product.cs
@@ -13,14 +13,16 @@ public class Product {
     [Required(ErrorMessage = "Informe a Descrição")] 
     public string Description { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Informe a Imagem")] 
+    //[Required(ErrorMessage = "Informe a Imagem")] 
     public string Image { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Informe o Preço")]
+    [Range(1, 1000, ErrorMessage = "Preço Deve Ser Entre R$ 1 e R$ 1.000")]
     public double Price { get; set; } 
     
     public int CategoryId { get; set; }
 
+    [Required(ErrorMessage = "Selecione a Categoria")] 
     public Category Category { get; set; } = new();
 
     public Product() { }
